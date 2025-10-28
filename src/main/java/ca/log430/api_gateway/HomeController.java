b37ee27aaea7895994f5465ebc7758dff7f4f77f
Rémi Alban <remi.alban.1@ens.etsmtl.ca>
2025-10-28 11:08:11 -0400

    add service status endpoint and configure Prometheus in docker-compose

--- src/main/java/ca/log430/api_gateway/HomeController.java
+++ src/main/java/ca/log430/api_gateway/HomeController.java
@@ -2,11 +2,40 @@ package ca.log430.api_gateway;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class HomeController {
+    private final RestTemplate restTemplate = new RestTemplate();
+
     @GetMapping("/")
     public String home() {
         return "API Gateway is running";
     }
+
+    @GetMapping("/status")
+    public Map<String, String> getServicesStatus() {
+        Map<String, String> result = new HashMap<>();
+
+
+        result.put("users", checkUrl("http://172.17.0.1:8080"));
+        result.put("transactions", checkUrl("http://172.17.0.1:8081"));
+        result.put("payments", checkUrl("http://172.17.0.1:8083"));
+
+        return result;
+    }
+
+    private String checkUrl(String url) {
+        try {
+            restTemplate.getForEntity(url, String.class);
+            return "OK";
+        } catch (Exception e) {
+            return "DOWN";
+        }
+    }
+
+
 }
